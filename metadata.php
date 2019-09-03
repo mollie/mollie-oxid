ba0eb57b6d6ce162b13e37571afaf29fbddc5e1a
Robert Müller <robert.mueller@fatchip.de>
2019-09-03 18:51:27 +0200

    Increased version number for new release

--- metadata.php
+++ metadata.php
@@ -19,7 +19,7 @@ $aModule = [
         'en' => '',
     ],
     'thumbnail'    => 'mollie_logo.png',
-    'version'       => '1.0.4',
+    'version'       => '1.0.5',
     'author'        => 'Fatchip GmbH',
     'email'         => 'info@mollie.com',
     'url'          => 'https://www.mollie.com/',
