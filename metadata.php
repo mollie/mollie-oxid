87edfd1995f61eee137a298385324e96e3ce6838
markusmichalski-fc <mm$git1972>
2021-03-09 16:01:18 +0100

    added latest version to metadata.php and composer.json

--- metadata.php
+++ metadata.php
@@ -40,7 +40,7 @@ not because a contract forces you!
 ",
     ],
     'thumbnail'    => 'mollie_logo.png',
-    'version'       => '1.0.18',
+    'version'       => '1.0.19',
     'author'        => 'Fatchip GmbH',
     'email'         => 'info@mollie.com',
     'url'          => 'https://www.mollie.com/',
