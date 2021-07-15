a21daaa973130425333663a4e570b6dbf9392e71
markusmichalski-fc <mm$git1972>
2021-07-15 13:54:25 +0200

    added latest version to metadata.php and composer.json

--- metadata.php
+++ metadata.php
@@ -64,7 +64,7 @@ fees op opstartkosten. Je betaalt alleen voor succesvolle transacties.
 ",
     ],
     'thumbnail'    => 'mollie_logo.png',
-    'version'       => '1.0.22',
+    'version'       => '1.0.23',
     'author'        => 'Fatchip GmbH',
     'email'         => 'info@mollie.com',
     'url'          => 'https://www.mollie.com/',
