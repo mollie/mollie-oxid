b2876b24f6c47c9150a319f7b990a97e85fb500d
Robert Müller <robert.mueller@fatchip.de>
2023-10-16 12:35:47 +0200

    PIOXD-187 - Fixed problem with failed status detection

--- extend/Application/Controller/OrderController.php
+++ extend/Application/Controller/OrderController.php
@@ -75,8 +75,8 @@ class OrderController extends OrderController_parent
             }
 
             $aResult = $oOrder->mollieGetPaymentModel()->getTransactionHandler($oOrder)->processTransaction($oOrder, 'success');
-            Registry::getLogger()->critical(print_r($aResult,true));
-            if ($aResult['success'] === false && $aResult['type'] !== 'success' ) {
+
+            if ($aResult['success'] === false) {
                 Registry::getSession()->deleteVariable('sess_challenge');
 
                 $sErrorIdent = 'MOLLIE_ERROR_SOMETHING_WENT_WRONG';
