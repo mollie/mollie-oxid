6fe7df12a72b8b2d9a399f6b3cea84a4b070325f
Robert Müller <robert.mueller@fatchip.de>
2024-03-27 12:22:58 +0100

    PIOXD-230 - Fixed auth mode for credit card payment

--- Application/Model/TransactionHandler/Payment.php
+++ Application/Model/TransactionHandler/Payment.php
@@ -39,6 +39,10 @@ class Payment extends Base
             $blSuccess = true;
         }
 
+        if ($oTransaction->isAuthorized()) {
+            $blSuccess = true;
+        }
+
         if ($oTransaction->hasRefunds()) {
             $blSuccess = true;
         }
