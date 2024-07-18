82200237787f56a0f361e05b2bab2d4699564018
Markus Michalski <62998217+markusmichalski-fc@users.noreply.github.com>
2024-07-18 06:49:02 +0200

    Update metadata.php

--- metadata.php
+++ metadata.php
@@ -88,7 +88,7 @@ ni costes de configuración. Solo va a pagar por las transacciones exitosas. Y s
 ",
     ],
     'thumbnail'    => 'mollie_logo.png',
-    'version'       => '1.0.36',
+    'version'       => '1.0.37',
     'author'        => 'Fatchip GmbH',
     'email'         => 'info@mollie.com',
     'url'          => 'https://www.mollie.com/',
