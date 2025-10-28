f7a766be188b1be3bc3cb477ab2d2b8942f95ef4
Robert Müller <robert.mueller@fatchip.de>
2025-10-28 12:07:12 +0100

    Fixed problem with missing items in order email when they were the last item in stock (#43)

--- extend/Application/Model/BasketItem.php
+++ extend/Application/Model/BasketItem.php
@@ -2,6 +2,8 @@
 
 namespace Mollie\Payment\extend\Application\Model;
 
+use Mollie\Payment\Application\Model\Cronjob\FinishOrders;
+
 class BasketItem extends BasketItem_parent
 {
     /**
@@ -17,5 +19,29 @@ class BasketItem extends BasketItem_parent
     public function mollieUnsetArticle()
     {
         $this->_oArticle = null;
+    }
+
+    /**
+     * Retrieves the article .Throws an exception if article does not exist,
+     * is not buyable or visible.
+     *
+     * @param bool   $blCheckProduct       checks if product is buyable and visible
+     * @param string $sProductId           product id
+     * @param bool   $blDisableLazyLoading disable lazy loading
+     *
+     * @throws oxArticleException exception in case of no current object product id is set
+     * @throws oxNoArticleException exception in case if product not exitst or not visible
+     * @throws oxArticleInputException exception if product is not buyable (stock and so on)
+     *
+     * @return \OxidEsales\Eshop\Application\Model\Article|oxOrderArticle
+     */
+    public function getArticle($blCheckProduct = false, $sProductId = null, $blDisableLazyLoading = false)
+    {
+        if (FinishOrders::mollieIsFinishingOrder() === true) {
+            // We don't need to check product when finishing order - item was already bought and buyability was checked
+            // Order emails may be incomplete - with missing products - when $blCheckProduct is true
+            $blCheckProduct = false;
+        }
+        return parent::getArticle($blCheckProduct, $sProductId, $blDisableLazyLoading);
     }
 }
