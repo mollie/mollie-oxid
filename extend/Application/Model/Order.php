8a96257f6253cf3829d27bc79d1eccad2f354b98
Robert Müller <robert.mueller@fatchip.de>
2026-02-23 17:53:30 +0100

    PIOXD-393 - Fixed problem with order expiry cronjob and authorized orders

--- extend/Application/Model/Order.php
+++ extend/Application/Model/Order.php
@@ -665,9 +665,9 @@ class Order extends Order_parent
      *
      * @return object|false
      */
-    public function mollieGetTransaction()
+    public function mollieGetTransaction($blUseCache = true)
     {
-        if ($this->mollieTransaction === null) {
+        if ($this->mollieTransaction === null || $blUseCache === false) {
             $oPaymentModel = $this->mollieGetPaymentModel();
             try {
                 $this->mollieTransaction = $oPaymentModel->getApiEndpointByOrder($this)->get($this->oxorder__oxtransid->value, ["embed" => "payments"]);
@@ -928,7 +928,7 @@ class Order extends Order_parent
                 return true;
             }
 
-            $oTransaction = $this->mollieGetTransaction();
+            $oTransaction = $this->mollieGetTransaction(false);
             if ($oTransaction->isAuthorized() === true) {
                 return true;
             }
@@ -941,7 +941,7 @@ class Order extends Order_parent
      */
     public function mollieCanCancelOrderBecauseExpired()
     {
-        $oTransaction = $this->mollieGetTransaction();
+        $oTransaction = $this->mollieGetTransaction(false);
         if ($oTransaction->isAuthorized() === true || $oTransaction->isPaid() === true) {
             return false;
         }
